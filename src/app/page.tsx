807269a97babf1bda195508eaba3f22c662c8b39
harith-alsafi <harith.alsafi@gmail.com>
2023-08-24 11:36:51 +0100

    VELZ-670: Structure
    Installed the main needed components for creating the UI using ShadCN UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="overflow-hidden p-3">
-        <Button>Button</Button>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button> Hi world
+        </Button>
     </main>
   )
 }
